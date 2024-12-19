56a2b76af1dd8573c48a5434637e314bc23b484d
sophia-banou <sdi2000136@di.uoa.gr>
2024-12-19 11:43:10 +0200

    another try

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sophia-banou.github.io",
-  base: "space-tourism",
+  base: "/space-tourism/",
   prefetch: true,
 });
